97d9106c6b3fcca9d07e1902662cf7d57c90032f
David Sheinbein <davesheinbein@gmail.com>
2020-09-06 04:05:33 -0700

    update modal created

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import {
 	Image,
 	SectionList,
 	Button,
+	Modal,
+	TouchableHighlight,
 } from 'react-native';
 import Constants from 'expo-constants';
 
@@ -38,12 +40,21 @@ const Item = ({ title }) => (
 );
 
 export default class App extends Component {
+	state = {
+		modalVisible: false,
+	};
+
+	setModalVisible = (visible) => {
+		this.setState({ modalVisible: visible });
+	};
+
 	onPressLearnMore() {
 		<View style={styles.learnMore}>
 			<Text style={styles.learnMoreText}>Hello</Text>
 		</View>;
 	}
 	render() {
+		const { modalVisible } = this.state;
 		return (
 			<SafeAreaView style={styles.container}>
 				<Text style={styles.heading}>
@@ -155,6 +166,46 @@ export default class App extends Component {
 								accessibilityLabel='Learn more about this purple button'
 							/>
 						</View>
+						<View style={styles.centeredView}>
+							<Modal
+								animationType='slide'
+								transparent={true}
+								visible={modalVisible}
+								onRequestClose={() => {
+									Alert.alert('Modal has been closed.');
+								}}>
+								<View style={styles.centeredView}>
+									<View style={styles.modalView}>
+										<Text style={styles.modalText}>
+											Hello World!
+										</Text>
+
+										<TouchableHighlight
+											style={{
+												...styles.openButton,
+												backgroundColor: '#2196F3',
+											}}
+											onPress={() => {
+												this.setModalVisible(!modalVisible);
+											}}>
+											<Text style={styles.textStyle}>
+												Hide Modal
+											</Text>
+										</TouchableHighlight>
+									</View>
+								</View>
+							</Modal>
+
+							<TouchableHighlight
+								style={styles.openButton}
+								onPress={() => {
+									this.setModalVisible(true);
+								}}>
+								<Text style={styles.textStyle}>
+									Show Modal
+								</Text>
+							</TouchableHighlight>
+						</View>
 					</ScrollView>
 				</View>
 			</SafeAreaView>
@@ -257,7 +308,8 @@ const styles = StyleSheet.create({
 		color: '#f9c2ff',
 		justifyContent: 'center',
 		alignContent: 'center',
-		// left: '42%',
+		left: '40%',
+		width: '100%',
 		marginVertical: 8,
 		position: 'relative',
 		padding: 10,
@@ -281,5 +333,41 @@ const styles = StyleSheet.create({
 		marginVertical: 8,
 		padding: 10,
 		width: '100%',
+	},
+	centeredView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 22,
+	},
+	modalView: {
+		margin: 20,
+		backgroundColor: 'white',
+		borderRadius: 20,
+		padding: 35,
+		alignItems: 'center',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+	},
+	openButton: {
+		backgroundColor: '#F194FF',
+		borderRadius: 20,
+		padding: 10,
+		elevation: 2,
+	},
+	textStyle: {
+		color: 'white',
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	modalText: {
+		marginBottom: 15,
+		textAlign: 'center',
 	},
 });
